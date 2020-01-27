86a4b940481be12321c7a176fafb3f65765cd540
SajeevRaj5 <sajeevraj.1989@gmail.com>
2020-01-27 12:02:39 +0400

    Added project files

---
+++ ImageUploaderSample/ImageUploaderSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageUploaderSample
+//
+//  Created by Sajeev on 1/27/20.
+//  Copyright © 2020 ImageUploaderSample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
