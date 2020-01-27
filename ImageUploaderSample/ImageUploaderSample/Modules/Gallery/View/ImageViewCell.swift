6e348a782a2845dfea9e20b371497715b841ebf6
SajeevRaj5 <sajeevraj.1989@gmail.com>
2020-01-27 12:13:24 +0400

    Designed Gallery View custom collection cell

---
+++ ImageUploaderSample/ImageUploaderSample/Modules/Gallery/View/ImageViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImageViewCell.swift
+//  ImageUploaderSample
+//
+//  Created by Sajeev on 1/27/20.
+//  Copyright © 2020 ImageUploaderSample. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var galleryImageView: UIImageView?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
