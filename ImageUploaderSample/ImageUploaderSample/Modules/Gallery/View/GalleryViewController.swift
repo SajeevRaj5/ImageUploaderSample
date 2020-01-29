0e0cfe155ec3b6d13a86311b2da54c684834965d
SajeevRaj5 <sajeevraj.1989@gmail.com>
2020-01-29 10:05:16 +0400

    Implemented the collection delegates and added the image fetching trigger calls

--- ImageUploaderSample/ImageUploaderSample/Modules/Gallery/View/GalleryViewController.swift
+++ ImageUploaderSample/ImageUploaderSample/Modules/Gallery/View/GalleryViewController.swift
@@ -11,19 +11,40 @@ import UIKit
 class GalleryViewController: UIViewController {
     
     var presenter: ViewToPresenterGalleryViewProtocol?
+    
+    let cellsPerRow = 2
+    
+    // to check if service is in progress
+    var isDataLoading: Bool = false
+    
+    var imageUrls = [URL]() {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                self?.galleryCollectionView?.reloadData()
+            }
+        }
+    }
 
     @IBOutlet weak var galleryCollectionView: UICollectionView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        registerCell()
+        
+        isDataLoading = true
+        presenter?.fetchGalleryItems()
+    }
+    
+    private func registerCell() {
+        galleryCollectionView?.register(UINib(nibName: ImageViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: ImageViewCell.identifier)
     }
 
 }
 
 extension GalleryViewController: PresenterToViewGalleryViewProtocol {
-    func showImages() {
-        
+    func showImages(imageUrls: [URL]) {
+        self.imageUrls += imageUrls
     }
     
     func showError(error: Error) {
@@ -31,8 +52,35 @@ extension GalleryViewController: PresenterToViewGalleryViewProtocol {
     }
     
     func dismissLoader() {
-        
+        isDataLoading = false
     }
+    
+}
+
+extension GalleryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageUrls.count
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageViewCell.identifier, for: indexPath) as? ImageViewCell else { return UICollectionViewCell() }
+        cell.configure(url: imageUrls[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        // if user scroll to last 3 data in the list, start fetching the next list from next page
+        guard (indexPath.row == imageUrls.count - 1), !isDataLoading else { return }
+        presenter?.fetchGalleryItems()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let view = galleryCollectionView else { return CGSize(width: 0, height: 0) }
+        
+         let marginsAndInsets = 5 * 2 + view.safeAreaInsets.left + view.safeAreaInsets.right + 5 * CGFloat(cellsPerRow - 1)
+         let itemWidth = ((view.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow)).rounded(.down)
+         return CGSize(width: itemWidth, height: itemWidth)
+     }
     
 }
 
