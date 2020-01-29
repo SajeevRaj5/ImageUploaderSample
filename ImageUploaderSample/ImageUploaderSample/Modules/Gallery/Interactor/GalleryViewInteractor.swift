932a40cc27836fbf7343924eb20ff8f580fc47c8
SajeevRaj5 <sajeevraj.1989@gmail.com>
2020-01-29 09:35:13 +0400

    Implemented fetching of images and handling completions in interactor class

--- ImageUploaderSample/ImageUploaderSample/Modules/Gallery/Interactor/GalleryViewInteractor.swift
+++ ImageUploaderSample/ImageUploaderSample/Modules/Gallery/Interactor/GalleryViewInteractor.swift
@@ -11,9 +11,22 @@ import Foundation
 class GalleryViewInteractor: PresenterToInteractorGalleryViewProtocol {
     weak var presenter: InteractorToPresenterGalleryViewProtocol?
     
+    var nextIndex = ""
+    
     func fetchImages() {
-        
+        GalleryItem.getAllImages(index: nextIndex) { [weak self] (response) in
+            guard let welf = self else { return }
+            switch response {
+            case .success(let result):
+                welf.nextIndex = result.nextIndex ?? ""
+                welf.presenter?.onSuccessImagesFetch(items: result.resources ?? [])
+            case .failure(let error):
+                welf.presenter?.onFailedImagesFetch(error: error)
+            case .finally:
+                welf.presenter?.onServerResponseReceival()
+            }
+        }
     }
     
-    
 }
+
