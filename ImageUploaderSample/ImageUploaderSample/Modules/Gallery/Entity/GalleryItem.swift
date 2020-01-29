ed6981999b6e69f605080bf3785d45f0541d1534
SajeevRaj5 <sajeevraj.1989@gmail.com>
2020-01-29 09:59:08 +0400

    Implemented the Entity class

--- ImageUploaderSample/ImageUploaderSample/Modules/Gallery/Entity/GalleryItem.swift
+++ ImageUploaderSample/ImageUploaderSample/Modules/Gallery/Entity/GalleryItem.swift
@@ -8,6 +8,53 @@
 
 import Foundation
 
-class GalleryItem {
+class GalleryItem: Codable {
+    let bytes : Int?
+    let format : String?
+    let height : Int?
+    let type : String?
+    let url : String?
+    let version : Int?
+    let width : Int?
     
+    static func getAllImages(index: String, completion: @escaping (ServiceResponse<GalleryResponse>) -> ()) {
+        Router.images(index: index).request { (response: ServiceResponse<GalleryResponse>) in
+            switch response {
+            case .success(let result):
+                completion(.success(data: result))
+            case .failure(let error):
+                completion(.failure(error: error))
+            case .finally:
+                completion(.finally)
+            }
+        }
+    }
+}
+
+extension GalleryItem {
+    enum Router: Requestable {
+        case images(index: String)
+        
+        var path: String {
+            return "resources/image"
+        }
+        
+        var parameters: [String : String] {
+            switch self {
+            case .images(let nextPageIndex):
+                return ["next_cursor": nextPageIndex]
+            }
+        }
+    }
+}
+
+class GalleryResponse: Codable {
+    let nextIndex : String?
+    let resources : [GalleryItem]?
+
+
+    enum CodingKeys: String, CodingKey {
+        case nextIndex = "next_cursor"
+        case resources = "resources"
+    }
 }
