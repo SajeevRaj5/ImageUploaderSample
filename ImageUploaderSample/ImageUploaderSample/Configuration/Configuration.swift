efacc55fc40f5a0413acd4687f27b6a31f1583ba
SajeevRaj5 <sajeevraj.1989@gmail.com>
2020-02-01 22:20:25 +0400

    Changed upload_preset to Configuration class

--- ImageUploaderSample/ImageUploaderSample/Configuration/Configuration.swift
+++ ImageUploaderSample/ImageUploaderSample/Configuration/Configuration.swift
@@ -13,4 +13,5 @@ class Configuration {
     static let user         = "315764852339698"
     static let password     = "otu5NR3YGH27c6SXN-1YqMYXGK0"
     static let cloudName    = "dkcsv4kwt"
+    static let uploadPreset = "y8mqtnq1"
 }
