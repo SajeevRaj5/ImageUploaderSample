6aad351aa3e7909277ed2357c93e29fd7f72e127
Sajeev Raj <sajeevraj.1989@gmail.com>
2020-02-04 01:00:34 +0400

    folder stucture refactor

--- ImageUploaderSample/Pods/Target Support Files/Pods-ImageUploaderSample-ImageUploaderSampleUITests/Pods-ImageUploaderSample-ImageUploaderSampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ImageUploaderSample_ImageUploaderSampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ImageUploaderSample_ImageUploaderSampleUITests
-@end
